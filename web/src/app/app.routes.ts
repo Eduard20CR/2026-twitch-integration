fb3ebf671a6f3c339870b4fae288f629937d5b9e
Scarus <41557299+Eduard20CR@users.noreply.github.com>
2026-06-27 15:24:01 +0100

    Organize Angular modules: create home, game, and profile modules with routing, components, and styles; update app routing to lazy load modules.

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
-import { App } from './app';
 
 export const routes: Routes = [
     {
-        path: 'dashboard',
-        component: App
+        path: '',
+        loadChildren: () =>
+            import('./modules/home/home.routes')
+                .then(m => m.HOME_ROUTES)
+    },
+    {
+        path: 'app',
+        loadChildren: () =>
+            import('./modules/game/game.routes')
+                .then(m => m.GAME_ROUTES)
+    },
+    {
+        path: 'profile',
+        loadChildren: () =>
+            import('./modules/profile/profile.routes')
+                .then(m => m.PROFILE_ROUTES)
     }
 ];
---
+++ web/src/app/modules/game/game.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Game } from './game';
+
+export const GAME_ROUTES: Routes = [
+    {
+        path: '',
+        component: Game
+    }
+];
