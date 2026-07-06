48ce2c1d10bd04ee60eb8596d7f35452ee918819
Scarus <41557299+Eduard20CR@users.noreply.github.com>
2026-07-06 19:39:35 +0100

    Add global Angular components: integrate Footer and DaisyUI, update Navbar, and refactor app structure

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Auth } from './core/services/auth';
 import { Navbar } from './core/layout/navbar/navbar';
+import { Footer } from './core/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
