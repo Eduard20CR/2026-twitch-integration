de495ef09f28520b937c77800f8be2fad6078640
Eduard20CR <educhito20@gmail.com>
2026-07-09 19:24:52 +0100

    Add ping event handling in WebSocket message receiver

--- server/modules/chat/router.py
+++ server/modules/chat/router.py
@@ -51,6 +51,9 @@ async def receive_messages(ws_connection: WsConnection, current_user):
                 case "leave_chat_room":
                     ws_manager.remove_user_from_room(ws_connection.user_id, ws_connection.user_id)
 
+                case "ping":
+                    print(f"Received ping from {ws_connection.id}")
+
                 case _:
                     print(f"Unknown event: {event}")
 
